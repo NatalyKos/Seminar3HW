8d83bdcc33156cddaad1a861a05b8f4eacd9a267
Nataly K <doublejanus4@gmail.com>
2022-08-18 19:10:36 +0300

    initial

---
+++ Ass1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и 
+// проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да//
+
+int input = ReadInt("Введите пятизначное число: ");
+int last = input % 10;
+int count = 10000;
+while(count > 100)
+{
+        if(last == input / count % 10 && input > 10)
+        {
+            input = input - last*count;
+            last = input / 10 % 10;
+            count = count / 10;
+            Console.WriteLine("Yes");
+            break;
+        }
+        else
+        {
+        Console.WriteLine("No");
+        break;
+        }
+
+}
+
+
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
---
+++ Ass3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+int input = ReadInt("Введите число: ");
+int count = 1;
+while(count <= input)
+{
+    Console.Write(Cube(count) + " ");
+    count++;
+}
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Cube(int x)
+{
+    return x*x*x;
+}
